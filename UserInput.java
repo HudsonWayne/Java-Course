ec8657035696a96e8e2cdb3a640667166f8edfe9
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-14 13:32:14 +0200

    inputs

--- UserInput.java
+++ UserInput.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
 
 public class UserInput {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+            System.err.println("Enter your name:");
+            String name = scanner.nextLine();
+
+            System.out.print("Enter your age: ");
+            int age = input.nextInt();
+    }
 }
