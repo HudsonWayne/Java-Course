5b49bb25544d9e94243ce01c6b32e2ad502cbb29
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-14 15:03:18 +0200

    finished the age checklist

--- AgeCheck.java
+++ AgeCheck.java
@@ -1,5 +1,20 @@
 import java.util.Scanner;
 
 public class AgeCheck {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+
+        if (age < 13) {
+            System.out.println("You are a child.");
+        } else if (age < 20) {
+            System.out.println("You are a teenager.");
+        } else {
+            System.out.println("You are an adult.");
+        }
+
+        input.close();
+    }
 }
