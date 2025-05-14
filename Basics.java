97ecc70010e94c7f2cf345357721a46abe651f43
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-14 12:01:08 +0200

    assigning variables

---
+++ Basics.java
@@ -0,0 +1,9 @@
+public class Basics {
+    public static void main(String[] args) {
+        int age = 30;
+        double height = 1.75;
+        String name =  "Wayne";
+        Boolean isAnInstructor = true;
+
+    }
+}
