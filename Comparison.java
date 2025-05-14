993df9ba2ad716082988ccc2b2fae829cf7e481e
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-14 12:47:47 +0200

    finished the the comparison operators

---
+++ Comparison.java
@@ -0,0 +1,11 @@
+public class Comparison {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+
+        System.out.println("x == y: " + (x == y));
+        System.out.println("x != y: " + (x != y));
+        System.out.println("x > y: " + (x > y));
+        System.out.println("x < y: " + (x < y));
+    }
+}
