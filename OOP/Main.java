3980dc863c08da2e4c90b0b18ca2756a89bf4594
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-15 14:03:51 +0200

    main to run the app

---
+++ OOP/Main.java
@@ -0,0 +1,12 @@
+package OOP;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create a person object
+        Person p1 = new Person();
+
+        //Set Values
+        p1.name = "Wayne";
+        p1.age = 25;
+    }
+}
--- OOP/Person.java
+++ OOP/Person.java
@@ -4,4 +4,9 @@ public class Person {
     //Fields Variables
     String name;
     int age;
+
+     // Method
+     void sayHello() {
+        System.out.println("Hi, my name is " + name + " and I am " + age + " years old.");
+    }
 }
