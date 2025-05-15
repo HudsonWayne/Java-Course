6abd6fecb56bba9cb693061620e3f5326950990b
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-15 11:11:51 +0200

    finished the odd and even

--- EvenOdd.java
+++ EvenOdd.java
@@ -4,6 +4,15 @@ public class EvenOdd {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.err.println("Enter a number");
+        System.out.println("Enter a number");
+        int number = input.nextInt();
+
+        if(number % 2 == 0) {
+            System.out.println(number + " is even");
+        }else{
+            System.out.println(number + " is odd");
+        }
+    
+        input.close();
     }
 }
