e718b7237b92f12c5cd9f4bcabac59a98ccfb368
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-15 11:54:13 +0200

    added in put in the do while file

--- DoWhileLoop.java
+++ DoWhileLoop.java
@@ -3,5 +3,11 @@ import java.util.Scanner;
 public class DoWhileLoop {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        String answer;
+
+        do{
+            System.out.println("Do you want to continue? (yes/no):");
+            answer = input.nextLine();
+        }
     }
 }
