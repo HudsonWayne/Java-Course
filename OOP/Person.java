8a2fb9bd5292389d8e29e49536376ec4dd555ec5
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-19 09:27:12 +0200

    adding constructors

--- OOP/Person.java
+++ OOP/Person.java
@@ -5,6 +5,12 @@ public class Person {
     String name;
     int age;
 
+    //Constructors is an automatic method that runs automatically when an object is created
+    public Person(String personName, int personAge) {
+        name = personName;
+        age = personAge;
+    }
+
      // Method
      void sayHello() {
         System.out.println("Hi, my name is " + name + " and I am " + age + " years old.");
