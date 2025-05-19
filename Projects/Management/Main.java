0bae920b760be550e60504ea07527c71d8b915cd
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-19 11:41:46 +0200

    adding a while condition:

--- Projects/Management/Main.java
+++ Projects/Management/Main.java
@@ -3,5 +3,10 @@ import java.util.Scanner;
 
 public class Main {
     Scanner scanner = new Scanner(System.in);
-    
+    Student[] students = new Student[100];
+    int count = 0;
+
+    while(true){
+        
+    }
 }
