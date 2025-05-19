a0f46af2cab35273b9c74c4a230dda3ba1f742cf
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-19 11:43:25 +0200

    added the print lines for all students

--- Projects/Management/Main.java
+++ Projects/Management/Main.java
@@ -7,6 +7,16 @@ public class Main {
     int count = 0;
 
     while(true){
-        
+        System.out.println("\n--- Student Management System ---");
+            System.out.println("1. Add Student");
+            System.out.println("2. View All Students");
+            System.out.println("3. Search by Name");
+            System.out.println("4. Exit");
+            System.out.print("Choose an option: ");
+            int option = scanner.nextInt();
+            scanner.nextLine(); // clear buffer
+
     }
 }
+
+
