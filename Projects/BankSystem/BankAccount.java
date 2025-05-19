8159b4af881f3e43dc8415e76b9cc87dcaccffd0
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-19 14:52:23 +0200

    adding a deposit method

--- Projects/BankSystem/BankAccount.java
+++ Projects/BankSystem/BankAccount.java
@@ -4,4 +4,13 @@ public class BankAccount {
     private String ownerName;
     private double balance;
 
+public BankAccount(String owenerName){
+    this.ownerName = ownerName;
+        this.balance = 0.0;  // Start with zero balance
+}    
+ //Deposit method
+
+public void Deposit(double amount){}
+
+
 }
