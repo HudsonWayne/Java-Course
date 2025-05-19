c93d5975233baaf82d11e1a0c056830c2adde143
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-19 10:26:51 +0200

    added my student arrays

--- OOP/Main.java
+++ OOP/Main.java
@@ -3,6 +3,11 @@ package OOP;
 public class Main {
     public static void main(String[] args) {
         Student[] students = new Student[3];
+
+        //Add students to the array
+        students[0] = new Student("John", 20);
+        students[1] = new Student("Alice", 22);
+        students[2] = new Student("Bob", 21);
     }
 
 
