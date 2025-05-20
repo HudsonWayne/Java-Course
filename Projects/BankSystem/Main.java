bef240da73ecde337ce0695614f08cd4c0a10ca0
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-20 10:37:51 +0200

    initial commit

--- Projects/BankSystem/Main.java
+++ Projects/BankSystem/Main.java
@@ -14,6 +14,22 @@ public class Main {
             System.out.println("4. Check Balance");
             System.out.println("5. Exit");
             System.out.print("Choose an option: ");
+
+            int option = scanner.nextInt();
+            scanner.nextInt();
+
+            if(option==1){
+                System.out.print("Enter your name: ");
+                String name = scanner.nextLine();
+                account = new BankAccount(name);
+                System.out.println("Account created for " + account.getOwnerName());
+            }else if(account == null){
+                System.out.println("Please create an account first.");
+            }else if(option == 2){
+                System.out.println("Enter amount to deposit: ");
+                double amount = scanner.nextDouble();
+                account.deposit(amount);
+            }
         }
     }
 
