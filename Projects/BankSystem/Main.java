e558addac759e01fd715a03fcfcd0dc0a1672513
HudsonWayne <waynewaynebenhura@gmail.com>
2025-05-20 09:46:33 +0200

    added the system prints

--- Projects/BankSystem/Main.java
+++ Projects/BankSystem/Main.java
@@ -5,6 +5,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         BankAccount account = null;
+
+        while (true) {
+            System.out.println("\n--- Bank System Menu ---");
+            System.out.println("1. Create Account");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Check Balance");
+            System.out.println("5. Exit");
+            System.out.print("Choose an option: ");
+        }
     }
+
     
 }
